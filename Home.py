80f2a080658493968266785d46f7600c8c9e9456
Gerardo Balboa <gerardobalboa@MacBook-Pro-di-Gerardo.local>
2023-07-17 14:58:52 +0200

    Iniziale commit del progetto

--- Home.py
+++ Home.py
@@ -1,30 +1,4 @@
-import streamlit as st
-import pandas as pd
-
-st.title("Boot to lead !")
-st.subheader("Ciao Ciccio")
-st.camera_input('label')
-
-data_df = pd.DataFrame(
-    {
-        "widgets": ["st.selectbox", "st.number_input", "st.text_area", "st.button"],
-        "favorite": [True, False, False, True],
-        "colore": ["verde", "rosso", "blu"]
-    }
-)
-
-st.data_editor(
-    data_df,
-    column_config={
-        "favorite": st.column_config.CheckboxColumn(
-            "Your favorite?",
-            help="Select your **favorite** widgets",
-            default=False,
 
 
-        )
-    },
-    disabled=["widgets"],
 
-    hide_index=True,
-)
+
