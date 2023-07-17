683dce3f04902cd7dd163a4611cd453c28953fce
Gerardo Balboa <gerardobalboa@MacBook-Pro-di-Gerardo.local>
2023-07-17 20:05:29 +0200

    soluzione?

--- Home.py
+++ Home.py
@@ -1,65 +1,44 @@
-import requests
-from bs4 import BeautifulSoup
-import pandas as pd
 import streamlit as st
-import base64
-
-
-def scrape_startup_data(url):
-    # Effettua la richiesta GET all'URL fornito dall'utente
-    response = requests.get(url)
-    soup = BeautifulSoup(response.content, 'html.parser')
-
-    # Trova la tabella contenente i risultati della ricerca
-    table = soup.find('table', class_='tab_data')
-
-    if table is None:
-        st.error("Nessun risultato trovato. Controlla l'URL o riprova più tardi.")
-        return None
-
-    # Estrai le intestazioni delle colonne dalla prima riga della tabella
-    header_row = table.find('tr')
-    headers = [header.text.strip() for header in header_row.find_all('th')]
-
-    # Crea una lista per memorizzare i dati delle startup
-    data = []
-
-    # Itera sulle righe della tabella (escludendo la prima riga delle intestazioni)
-    for row in table.find_all('tr')[1:]:
-        # Estrai i dati da ogni cella della riga
-        cell_data = [cell.text.strip() for cell in row.find_all('td')]
-        data.append(cell_data)
-
-    # Crea un DataFrame utilizzando i dati estratti e le intestazioni delle colonne
-    df = pd.DataFrame(data, columns=headers)
-    return df
-
+import pandas as pd
 
 def main():
-    st.title("Scraping Dati Startup Innovative")
+    st.title("Seleziona le colonne di interesse")
 
-    # Aggiungi input utente per l'URL
-    url = st.text_input("Inserisci l'URL:", "https://startup.registroimprese.it/isin/search?0#")
+    # Aggiungi input utente per il caricamento del file CSV
+    uploaded_file = st.file_uploader("Carica il file CSV", type=["csv"])
 
-    # Esegui lo scraping dei dati al clic del pulsante
-    if st.button("Esegui Scraping"):
-        st.text("Estrazione dei dati in corso...")
-        df = scrape_startup_data(url)
+    if uploaded_file is not None:
+        # Carica il file CSV in un DataFrame
+        df = pd.read_csv(uploaded_file)
 
-        if df is not None:
-            st.write("Dati delle startup innovative:")
-            st.dataframe(df)
+        # Mostra l'elenco delle colonne presenti nel file CSV
+        st.write("Colonne disponibili:")
+        st.write(df.columns)
 
-            # Salva il DataFrame in un file CSV al clic del pulsante "Download CSV"
-            if st.button("Download CSV"):
-                csv = df.to_csv(index=False)
-                b64 = base64.b64encode(csv.encode()).decode()  # Codifica in base64
-                href = f'<a href="data:file/csv;base64,{b64}" download="startup_data.csv">Clicca qui per scaricare il CSV</a>'
-                st.markdown(href, unsafe_allow_html=True)
+        # Aggiungi input utente per selezionare le colonne da mantenere
+        selected_columns = st.multiselect("Seleziona le colonne da mantenere", df.columns)
 
+        if len(selected_columns) > 0:
+            # Filtra il DataFrame per mantenere solo le colonne selezionate
+            df_selected = df[selected_columns]
+
+            # Mostra il DataFrame con le sole colonne selezionate
+            st.write("Dati con le colonne selezionate:")
+            st.write(df_selected)
+
+            # Salva il DataFrame con le colonne selezionate in un nuovo file CSV
+            csv = df_selected.to_csv(index=False)
+            b64 = base64.b64encode(csv.encode()).decode()  # Codifica in base64
+            href = f'<a href="data:file/csv;base64,{b64}" download="selected_columns_data.csv">Clicca qui per scaricare il CSV</a>'
+            st.markdown(href, unsafe_allow_html=True)
+        else:
+            st.warning("Seleziona almeno una colonna da mantenere.")
+    else:
+        st.warning("Carica un file CSV per iniziare.")
 
 if __name__ == '__main__':
     main()
+
 
 
 
