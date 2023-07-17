a517bb116c20fa23f4a8fe8f5f50720134634560
Gerardo Balboa <gerardobalboa@MacBook-Pro-di-Gerardo.local>
2023-07-17 20:17:23 +0200

    meglio

--- Home.py
+++ Home.py
@@ -18,27 +18,22 @@ def main():
         st.write("Colonne disponibili:")
         st.write(df.columns)
 
-        # Aggiungi input utente per selezionare le colonne da mantenere
-        selected_columns = st.multiselect("Seleziona le colonne da mantenere", df.columns)
+        # Aggiungi input utente per selezionare le colonne da visualizzare
+        selected_columns = st.multiselect("Seleziona le colonne da visualizzare", df.columns)
 
         if len(selected_columns) > 0:
             # Filtra il DataFrame per mantenere solo le colonne selezionate
             df_selected = df[selected_columns]
 
-            # Mostra il DataFrame con le sole colonne selezionate
-            st.write("Dati con le colonne selezionate:")
+            # Mostra il DataFrame con le colonne selezionate
+            st.write("Dati delle colonne selezionate:")
             st.write(df_selected)
-
-            # Salva il DataFrame con le colonne selezionate in un nuovo file CSV
-            csv = df_selected.to_csv(index=False)
-            st.download_button("Clicca qui per scaricare il CSV", data=csv, file_name="selected_columns_data.csv", mime="text/csv")
         else:
-            st.warning("Seleziona almeno una colonna da mantenere.")
+            st.warning("Seleziona almeno una colonna da visualizzare.")
     else:
         st.warning("Carica un file CSV per iniziare.")
 
 if __name__ == '__main__':
     main()
-
 
 
