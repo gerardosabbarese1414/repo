7a85f76430f7e5c257491403f65756aaa33e8f19
Gerardo Balboa <gerardobalboa@MacBook-Pro-di-Gerardo.local>
2023-07-18 17:32:05 +0200

    faed

--- Home.py
+++ Home.py
@@ -1,62 +1,91 @@
-import subprocess
-subprocess.run(["pip", "install", "--no-cache-dir", "-r", "requirements.txt"])
 import streamlit as st
 import pandas as pd
+import chromedriver_autoinstaller
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from webdriver_manager.chrome import ChromeDriverManager
 
-def get_emails(startup_list):
+# Funzione per estrarre i dati di tutte le startup, inclusi gli indirizzi email
+def extract_startup_data():
+    # Installa automaticamente il driver di Chrome
+    chromedriver_autoinstaller.install()
+
     options = webdriver.ChromeOptions()
     options.add_argument('--headless')  # Esegui Chrome in modalità headless (senza interfaccia grafica)
-    driver = webdriver.Chrome(executable_path=ChromeDriverManager().install(), options=options)
+    driver = webdriver.Chrome(options=options)
 
+    url = 'https://startup.registroimprese.it/isin/home'
+    driver.get(url)
+
+    # Seleziona il campo "Startup" e clicca sul tasto "Ricerca Avanzata"
+    driver.find_element_by_id('tipo_impresa').send_keys('startup')
+    driver.find_element_by_id('cercaAvanzata').click()
+
+    # Estrapola i dati di tutte le pagine
+    data = []
     emails = []
-    for startup in startup_list:
-        driver.get('https://startup.registroimprese.it/isin/home')
-
-        # Seleziona il campo startup e clicca sul tasto ricerca avanzata
-        driver.find_element_by_id('tipo_impresa').send_keys('startup')
-        driver.find_element_by_id('cercaAvanzata').click()
-
-        # Inserisci la regione nella ricerca
-        driver.find_element_by_id('sigla_regione').send_keys(startup)
-
+    while True:
         # Attendi il caricamento dei risultati
         WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, 'box_segnalazioni')))
 
-        # Estrapola le email dai risultati
-        emails_elem = driver.find_elements_by_xpath('//a[@class="mailto"]')
-        startup_emails = [email_elem.get_attribute('href').split(':')[1] for email_elem in emails_elem]
-        emails.extend(startup_emails)
+        # Estrapola i dati della pagina corrente
+        table = driver.find_element_by_class_name('tab_data')
+        rows = table.find_elements_by_tag_name('tr')
+        for row in rows[1:]:
+            cells = row.find_elements_by_tag_name('td')
+            startup_data = [cell.text for cell in cells]
+            data.append(startup_data)
+
+            # Recupera l'indirizzo email dalla pagina di ogni startup
+            link = cells[0].find_element_by_tag_name('a').get_attribute('href')
+            email = get_email_from_startup(link)
+            emails.append(email)
+
+        # Passa alla pagina successiva, se presente
+        next_button = driver.find_element_by_id('prossimo')
+        if next_button.get_attribute('class') == 'next disabled':
+            break
+        else:
+            next_button.click()
 
     driver.quit()
-    return emails
 
+    # Crea un DataFrame con i dati estratti
+    columns = ['Denominazione', 'Natura Giuridica', 'Codice Fiscale', 'Provincia', 'Comune', 'Data Iscrizione Startup', 'Data Iscrizione Registro Imprese', 'Data Inizio Attività', 'Ateco 2007', 'Settore', 'Attività', 'Sito Internet', 'Regione', 'Sezione Attività', 'Classe Addetti', 'Classe Val. Prod.', 'Alto Valore Tecnologico', 'Vocazione Sociale', 'Classe Capitale Sociale', 'Spese in Ricerca e Sviluppo', 'Forza Lavoro con Titoli', 'Possesso di Brevetti', 'Data Dichiarazione', 'Prevalenza Femminile', 'Prevalenza Giovanile', 'Prevalenza Straniera', 'Email']
+    df = pd.DataFrame(data, columns=columns)
+    df['Email'] = emails
+
+    return df
+
+# Funzione per estrarre l'email da ogni pagina di startup
+def get_email_from_startup(url):
+    options = webdriver.ChromeOptions()
+    options.add_argument('--headless')  # Esegui Chrome in modalità headless (senza interfaccia grafica)
+    driver = webdriver.Chrome(options=options)
+    driver.get(url)
+
+    # Trova l'elemento che contiene l'email
+    email_element = driver.find_element_by_xpath('//a[contains(@href, "mailto:")]')
+    email = email_element.get_attribute('href').split(':')[1]
+
+    driver.quit()
+    return email
+
+# Funzione principale dell'app Streamlit
 def main():
-    st.title("Bot per Estrarre Email di Startup Innovative")
+    st.title("Estrazione Dati Startup Innovative")
 
-    # Esempio: utilizziamo tutte le regioni italiane come elenco di ricerca
-    regioni = ['Abruzzo', 'Basilicata', 'Calabria', 'Campania', 'Emilia-Romagna', 'Friuli-Venezia Giulia', 'Lazio', 'Liguria', 'Lombardia', 'Marche', 'Molise', 'Piemonte', 'Puglia', 'Sardegna', 'Sicilia', 'Toscana', 'Trentino-Alto Adige', 'Umbria', 'Valle d\'Aosta', 'Veneto']
+    if st.button("Estrai Dati"):
+        st.text("Estrazione dei dati in corso...")
+        startup_data = extract_startup_data()
 
-    st.write("Seleziona le regioni per estrarre le email delle startup innovative:")
-    selected_regions = st.multiselect("Regioni", regioni, default=regioni)
+        st.write("Dati delle startup innovative:")
+        st.dataframe(startup_data)
 
-    if st.button("Estrai Email"):
-        startup_emails = get_emails(selected_regions)
-
-        st.write("Email delle startup trovate:")
-        for email in startup_emails:
-            st.write(email)
-
-        # Creazione DataFrame con le email
-        df = pd.DataFrame(startup_emails, columns=['Email'])
-
-        # Download CSV
-        csv_file = df.to_csv(index=False)
-        st.download_button("Scarica CSV", data=csv_file, file_name='startup_emails.csv', mime='text/csv')
+        # Scarica i dati in un file CSV
+        csv_file = startup_data.to_csv(index=False)
+        st.download_button("Scarica CSV", data=csv_file, file_name='startup_data.csv', mime='text/csv')
 
 if __name__ == '__main__':
     main()
