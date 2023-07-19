bf3cd5e78fb701c9b9a83f8ef595322b7bae9100
Gerardo Balboa <gerardobalboa@MacBook-Pro-di-Gerardo.local>
2023-07-19 16:56:09 +0200

    da

--- Home.py
+++ Home.py
@@ -4,7 +4,11 @@ import csv
 import streamlit as st
 import pandas as pd
 
+
 def get_first_email_from_website(url):
+    if not url.startswith("http://") and not url.startswith("https://"):
+        url = f"http://{url}"
+
     try:
         response = requests.get(url)
         if response.status_code == 200:
@@ -14,11 +18,12 @@ def get_first_email_from_website(url):
         pass
     return None
 
+
 def main():
     st.title("Web Scraping di Email da Siti Web")
-    st.write("Inserisci l'URL di un sito web senza HTTPS per cercare email.")
+    st.write("Inserisci l'URL di un sito web o solo il dominio per cercare email.")
 
-    url_list = st.text_area("Inserisci gli URL dei siti web (uno per riga)")
+    url_list = st.text_area("Inserisci gli URL dei siti web o domini (uno per riga)")
     urls = url_list.split("\n")
 
     if st.button("Cerca Email"):
@@ -27,13 +32,14 @@ def main():
 
         for url in urls:
             if url:
-                email = get_first_email_from_website(f"http://{url}")
+                email = get_first_email_from_website(url)
                 if email:
-                    results.append({"Sito Web": url, "Email Trovata": email})
+                    results.append({"Sito Web o Dominio": url, "Email Trovata": email})
 
         if results:
             df = pd.DataFrame(results)
             st.dataframe(df)
+
 
 if __name__ == "__main__":
     main()
