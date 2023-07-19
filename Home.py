dbc0e6453bc041a9f1f7402d5e685663ba03f559
Gerardo Balboa <gerardobalboa@MacBook-Pro-di-Gerardo.local>
2023-07-19 17:08:44 +0200

    dadad3waef

--- Home.py
+++ Home.py
@@ -4,11 +4,7 @@ import csv
 import streamlit as st
 import pandas as pd
 
-
 def get_first_email_from_website(url):
-    if not url.startswith("http://") and not url.startswith("https://"):
-        url = f"http://{url}"
-
     try:
         response = requests.get(url, timeout=10)
         if response.status_code == 200:
@@ -18,37 +14,41 @@ def get_first_email_from_website(url):
         pass
     return None
 
+def get_emails_from_text(text):
+    emails = re.findall(r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b', text)
+    return emails
 
 def contains_dot(text):
     return "." in text
 
-
 def main():
     st.title("Web Scraping di Email da Siti Web")
-    st.write("Inserisci l'URL di un sito web o solo il dominio per cercare email.")
+    st.write("Inserisci l'URL di un sito web, solo il dominio o del testo per cercare email.")
 
-    url_list = st.text_area("Inserisci gli URL dei siti web o domini (uno per riga)")
-    urls = url_list.split("\n")
+    user_input = st.text_area("Inserisci gli URL dei siti web o domini, o del testo (uno per riga)")
+    items = user_input.split("\n")
 
     if st.button("Cerca Email"):
         st.write("Risultati:")
         results = []
 
-        for url in urls:
-            url = url.strip()
-            if url:
-                email = get_first_email_from_website(url)
-                if email:
-                    results.append({"Sito Web o Dominio": url, "Email Trovata": email})
-                elif contains_dot(url):
-                    email = get_first_email_from_website(f"http://{url}")
+        for item in items:
+            item = item.strip()
+            if item:
+                # Search in websites or domains
+                if (item.startswith("http://") or item.startswith("https://")) or contains_dot(item):
+                    email = get_first_email_from_website(item)
                     if email:
-                        results.append({"Sito Web o Dominio": url, "Email Trovata": email})
+                        results.append({"Sito Web o Dominio": item, "Email Trovata": email})
+                # Search in plain text
+                else:
+                    emails = get_emails_from_text(item)
+                    for email in emails:
+                        results.append({"Testo": item, "Email Trovata": email})
 
         if results:
             df = pd.DataFrame(results)
             st.dataframe(df)
-
 
 if __name__ == "__main__":
     main()
